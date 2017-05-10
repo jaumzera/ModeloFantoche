4525604cc8b617182db88b118526060e9bb067fa
Jaumzera <joaomassan@gmail.com>
2017-05-09 22:23:47 -0300

    Testa o carregamento

---
+++ src/main/java/org/fantoche/main/Main.java
@@ -0,0 +1,38 @@
+package org.fantoche.main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.fantoche.modelofantoche.Classe;
+import org.fantoche.modelofantoche.Imagem;
+import org.fantoche.modelofantoche.SuperClasse;
+
+/**
+ *
+ * @author jaumzera
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        List<SuperClasse> especies = new ArrayList<>();
+        File raiz = new File("/home/jaumzera/Documents/mestrado_fantoche/teste");
+        for(File dir : raiz.listFiles()) {
+            if(dir.isDirectory() && !dir.getName().startsWith(".")) {
+                especies.add(SuperClasse.criar(dir));
+            }
+        }
+        
+        
+        for(SuperClasse especie : especies) {
+            System.out.println("Espécie: " + especie.getNome());
+            for(Classe classe : especie.getClasses()) {
+                System.out.println("\tClasses: " + classe.getNome());
+                for(Imagem imagem : classe.getImagens()) {
+                    System.out.println("\tImagem: " + imagem.getArquivo().getName());
+                }
+            }
+        }
+        
+    }
+}
